65514054490107c4a876d641fc59bf78da93d349
marianobura <marianoobura@gmail.com>
2025-09-04 21:00:10 -0300

    Add Navbar to Products Layout and update fetch cache policy in Products Page

--- frontend/src/app/products/layout.tsx
+++ frontend/src/app/products/layout.tsx
@@ -1,5 +1,11 @@
+import Navbar from '@/components/Navbar';
 import { ReactNode } from 'react';
 
 export default function ProductsLayout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
 }
--- frontend/src/app/products/page.tsx
+++ frontend/src/app/products/page.tsx
@@ -1,5 +1,4 @@
 import BaseText from '@/components/BaseText';
-import Navbar from '@/components/Navbar';
 import Link from 'next/link';
 
 type Product = {
@@ -10,7 +9,7 @@ type Product = {
 };
 
 async function getProducts(): Promise<Product[]> {
-  const res = await fetch('https://dummyjson.com/products?limit=0');
+  const res = await fetch('https://dummyjson.com/products?limit=0', { cache: 'no-cache' });
   const data = await res.json();
   return data.products;
 }
@@ -19,34 +18,31 @@ export default async function ProductsPage() {
   const products = await getProducts();
 
   return (
-    <>
-      <Navbar />
-      <div className="container py-3">
-        <div className="w-full text-center">
-          <BaseText variant="h2">Products Page</BaseText>
-        </div>
-        <div className="mt-2 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4">
-          {products.map((product) => (
-            <Link
-              href={`/products/${product.id}`}
-              key={product.id}
-              className="border-esona/20 flex flex-col border"
-            >
-              <img
-                src={product.thumbnail}
-                alt={`Imagen de ${product.title}`}
-                className="bg-esona/20 w-full object-none"
-              />
-              <div className="mt-2 flex grow flex-col justify-between gap-4 p-3 text-center">
-                <BaseText variant="h3" className="line-clamp-1 truncate">
-                  {product.title}
-                </BaseText>
-                <BaseText variant="text">${product.price}</BaseText>
-              </div>
-            </Link>
-          ))}
-        </div>
+    <div className="container py-3">
+      <div className="w-full text-center">
+        <BaseText variant="h2">Products Page</BaseText>
       </div>
-    </>
+      <div className="mt-2 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4">
+        {products.map((product) => (
+          <Link
+            href={`/products/${product.id}`}
+            key={product.id}
+            className="border-esona/20 flex flex-col border"
+          >
+            <img
+              src={product.thumbnail}
+              alt={`Imagen de ${product.title}`}
+              className="bg-esona/20 w-full object-none"
+            />
+            <div className="mt-2 flex grow flex-col justify-between gap-4 p-3 text-center">
+              <BaseText variant="h3" className="line-clamp-1 truncate">
+                {product.title}
+              </BaseText>
+              <BaseText variant="text">${product.price}</BaseText>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
   );
 }
