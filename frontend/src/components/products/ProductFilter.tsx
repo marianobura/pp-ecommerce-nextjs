37910b100c0c817e11359e35ace7d771e876fc0d
marianobura <marianoobura@gmail.com>
2025-09-09 02:40:10 -0300

    Enhance ProductFilter component with category selection and price reset functionality

--- frontend/src/components/products/ProductFilter.tsx
+++ frontend/src/components/products/ProductFilter.tsx
@@ -1,5 +1,5 @@
 import BaseText from '@/components/BaseText';
-import { RotateCcw } from 'lucide-react';
+import { Check, RotateCcw } from 'lucide-react';
 import { Category } from '@/types/category';
 
 export default function ProductFilter({
@@ -31,17 +31,66 @@ export default function ProductFilter({
                 />
               )}
             </div>
-            <div className="mr-8 mb-8 ml-2 flex max-h-44 flex-col gap-0.5 overflow-y-scroll">
+            <div className="mr-8 mb-8 flex max-h-44 flex-col gap-1 overflow-y-scroll">
               {categories.map((category) => (
                 <div
                   key={category.slug}
                   onClick={() => onSelectCategory(category.slug)}
-                  className={`cursor-pointer transition-colors ${
+                  className={`group flex cursor-pointer items-center gap-2 transition-colors ${
                     selectedCategory === category.slug
                       ? 'text-esona font-semibold'
                       : 'hover:text-esona text-neutral-700'
                   }`}
                 >
+                  <div
+                    className={`flex size-4 items-center justify-center rounded-4xl border transition-colors ${
+                      selectedCategory === category.slug
+                        ? 'border-esona bg-esona flex items-center justify-center'
+                        : 'group-hover:border-esona border-neutral-700'
+                    }`}
+                  >
+                    {selectedCategory === category.slug && (
+                      <Check size={12} strokeWidth={3} className="text-white" />
+                    )}
+                  </div>
+                  <BaseText variant="small">{category.name}</BaseText>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col gap-1 border-b border-neutral-200">
+            <div className="mr-8 flex items-center justify-between">
+              <BaseText variant="text-semibold">Price</BaseText>
+              {selectedCategory && (
+                <RotateCcw
+                  size={16}
+                  className="hover:text-esona cursor-pointer transition-colors"
+                  onClick={resetCategory}
+                />
+              )}
+            </div>
+            <div className="mr-8 mb-8 flex max-h-44 flex-col gap-1 overflow-y-scroll">
+              {categories.map((category) => (
+                <div
+                  key={category.slug}
+                  onClick={() => onSelectCategory(category.slug)}
+                  className={`group flex cursor-pointer items-center gap-2 transition-colors ${
+                    selectedCategory === category.slug
+                      ? 'text-esona font-semibold'
+                      : 'hover:text-esona text-neutral-700'
+                  }`}
+                >
+                  <div
+                    className={`flex size-4 items-center justify-center rounded-4xl border transition-colors ${
+                      selectedCategory === category.slug
+                        ? 'border-esona bg-esona flex items-center justify-center'
+                        : 'group-hover:border-esona border-neutral-700'
+                    }`}
+                  >
+                    {selectedCategory === category.slug && (
+                      <Check size={12} strokeWidth={3} className="text-white" />
+                    )}
+                  </div>
                   <BaseText variant="small">{category.name}</BaseText>
                 </div>
               ))}
