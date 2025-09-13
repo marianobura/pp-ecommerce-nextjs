dc81f3410910dfbd5acea06c5792f574f831070b
marianobura <marianoobura@gmail.com>
2025-09-13 01:13:21 -0300

    Add authentication layout and pages for login and registration

---
+++ frontend/src/app/auth/layout.tsx
@@ -0,0 +1,18 @@
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="grid min-h-dvh grid-cols-2">
+      <div className="bg-esona size-full max-h-dvh">
+        <img
+          src="/auth.jpg"
+          alt="Woman in yellow jacket being happy"
+          className="size-full object-cover object-bottom"
+        />
+      </div>
+      <div className="flex size-full items-center justify-center">
+        <div className="mx-16 w-full max-w-[500px] rounded-4xl border border-neutral-200 bg-white py-6">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -1,3 +1,51 @@
+import BaseButton from '@/components/BaseButton';
+import BaseInput from '@/components/BaseInput';
+import BaseText from '@/components/BaseText';
+import Logo from '@/components/icons/Logo';
+import Link from 'next/link';
+
 export default function RegisterPage() {
-  return <div>Register Page</div>;
+  return (
+    <>
+      <div className="mb-4 flex flex-col gap-2 border-b border-neutral-200 px-6 pb-4">
+        <div className="flex items-center justify-between gap-12">
+          <BaseText variant="h2">Create an account</BaseText>
+          <Logo className="h-auto w-32 shrink-0" />
+        </div>
+        <BaseText variant="small">
+          If you already have an account, you can{' '}
+          <Link href="/auth/login" className="text-esona underline">
+            sign in here
+          </Link>
+          .
+        </BaseText>
+      </div>
+      <BaseInput
+        label="Name"
+        type="text"
+        placeholder="Enter your full name"
+        className="mb-4 px-6"
+      />
+      <BaseInput label="Email" type="email" placeholder="Enter your email" className="mb-4 px-6" />
+      <BaseInput
+        label="Password"
+        type="password"
+        placeholder="Enter your password"
+        className="mb-4 px-6"
+        password
+      />
+      <BaseInput
+        label="Confirm password"
+        type="password"
+        placeholder="Confirm your password"
+        className="mb-4 px-6"
+        password
+      />
+      <div className="px-6">
+        <BaseButton variant="primary" className="w-full">
+          Create account
+        </BaseButton>
+      </div>
+    </>
+  );
 }
