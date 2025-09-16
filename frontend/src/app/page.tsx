dc6059b7bcde885cb5503133ce0b79d38d1d0945
marianobura <marianoobura@gmail.com>
2025-09-16 01:38:46 -0300

    Refactor homepage layout and update muted color in globals.css

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,10 +13,10 @@ export default async function Home() {
     <>
       <Navbar />
       <main className="container pt-1 pb-3">
-        <div className="bg-primary relative flex h-[600px] flex-col justify-center rounded-4xl p-12">
+        {/* <div className="relative flex h-[600px] flex-col justify-center rounded-4xl bg-neutral-100 p-12">
           <Hero className="absolute right-32 bottom-0 h-[500px]" />
-          <BaseText variant="h1" className="line-clamp-2 max-w-[700px] text-balance">
-            Everything You Need, All in <span className="text-primary leading-18">One Place</span>
+          <BaseText variant="h1" className="line-clamp-2 max-w-[700px] leading-14 text-balance">
+            Everything You Need, All in <span className="text-primary">One Place</span>
           </BaseText>
           <BaseText variant="text" className="max-w-[500px] text-pretty">
             Discover a wide range of products at unbeatable prices. From electronics to fashion, we
@@ -25,6 +25,44 @@ export default async function Home() {
           <BaseButton variant="primary" href="/products" className="mt-6 w-fit">
             Explore More
           </BaseButton>
+        </div> */}
+        {/* <div className="mt-12 flex h-[500px] flex-col justify-end gap-4 lg:mt-24">
+          <div className="w-full">
+            <BaseText variant="h1" className="w-1/2 leading-14 text-balance">
+              Everything You Need, All in <span className="text-primary">One Place</span>!
+            </BaseText>
+          </div>
+          <div className="bg-primary relative flex h-fit flex-col gap-6 rounded-4xl p-12">
+            <div className="w-full">
+              <BaseText variant="text" className="w-2/5 max-w-[600px] text-pretty text-white">
+                Discover a wide range of products at unbeatable prices. From electronics to fashion,
+                we have it all. Shop now and experience the convenience of one-stop shopping!
+              </BaseText>
+            </div>
+            <BaseButton variant="secondary" href="/products" className="w-fit">
+              Explore More
+            </BaseButton>
+            <Hero className="absolute right-8 bottom-0 h-[500px] md:right-16 xl:right-32" />
+          </div>
+        </div> */}
+        <div className="mt-12 flex gap-4 lg:mt-24">
+          <div className="flex basis-2/3 flex-col gap-4">
+            <div className="rounded-5xl bg-secondary/20 flex grow-1 items-center p-12">
+              <BaseText variant="h1" className="max-w-[700px] leading-14 text-balance">
+                Everything You Need, All in <span className="text-primary">One Place</span>!
+              </BaseText>
+            </div>
+            <div className="bg-primary rounded-5xl flex flex-col gap-6 p-12">
+              <BaseText variant="text" className="max-w-[500px] text-pretty text-white">
+                Discover a wide range of products at unbeatable prices. From electronics to fashion,
+                we have it all. Shop now and experience the convenience of one-stop shopping!
+              </BaseText>
+              <BaseButton variant="secondary" href="/products" className="w-fit">
+                Explore More
+              </BaseButton>
+            </div>
+          </div>
+          <div className="rounded-5xl min-h-[500px] basis-1/3 bg-neutral-100"></div>
         </div>
         <div className="no-scrollbar mt-4 flex w-full gap-4 overflow-auto">
           {categories.map((category) => (
