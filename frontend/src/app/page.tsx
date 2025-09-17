edc2166f69260baee796d4977590e1d984c3906c
marianobura <marianoobura@gmail.com>
2025-09-16 23:45:07 -0300

    Refactor layout and styling in Home component; remove unused Hero2 import and adjust spacing and order classes.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,6 @@ import Hero from '@/components/icons/Hero';
 import { MoveRight } from 'lucide-react';
 import Link from 'next/link';
 import { getCategories } from '@/services/products';
-import Hero2 from '@/components/icons/Hero2';
 
 export default async function Home() {
   const categories = await getCategories();
@@ -14,15 +13,18 @@ export default async function Home() {
     <>
       <Navbar />
       <main className="container pt-1 pb-3">
-        <div className="mt-8 flex gap-4 lg:mt-16">
-          <div className="flex basis-3/5 flex-col gap-4">
-            <div className="rounded-5xl bg-secondary/20 flex grow-1 items-center p-12">
-              <BaseText variant="h1" className="max-w-[700px] leading-14 text-balance">
+        <div className="mt-2 flex flex-col gap-4 md:mt-8 md:flex-row lg:mt-16">
+          <div className="order-2 flex flex-col gap-4 md:order-1 md:basis-3/5">
+            <div className="rounded-5xl bg-secondary/20 flex grow-1 items-center p-6 md:p-12">
+              <BaseText
+                variant="h1"
+                className="max-w-[700px] text-center leading-14 text-balance md:text-left"
+              >
                 Everything You Need, All in <span className="text-primary">One Place</span>!
               </BaseText>
             </div>
-            <div className="bg-primary rounded-5xl flex flex-col gap-6 p-12">
-              <BaseText variant="text" className="max-w-[500px] text-pretty text-white">
+            <div className="bg-primary rounded-5xl flex flex-col gap-3 p-6 md:gap-6 md:p-12">
+              <BaseText variant="text" className="text-pretty text-white md:max-w-[500px]">
                 Discover a wide range of products at unbeatable prices. From electronics to fashion,
                 we have it all. Shop now and experience the convenience of one-stop shopping!
               </BaseText>
@@ -33,8 +35,8 @@ export default async function Home() {
               </div>
             </div>
           </div>
-          <div className="rounded-5xl flex min-h-[500px] basis-2/5 items-center justify-center overflow-hidden bg-neutral-100">
-            <Hero2 className="h-[500px]" />
+          <div className="rounded-5xl order-1 flex items-center justify-center overflow-hidden bg-neutral-100 md:order-2 md:min-h-[500px] md:basis-2/5">
+            <Hero className="max-h-64 p-6 md:h-[500px] md:max-h-none md:p-12" />
           </div>
         </div>
         <div className="no-scrollbar mt-4 flex w-full gap-4 overflow-auto">
