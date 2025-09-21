4876cc0da0bfbe28e6593376fe05ae0aaedb0143
marianobura <marianoobura@gmail.com>
2025-09-21 18:45:47 -0300

    Update loading component styles for improved responsiveness and visual consistency

--- frontend/src/app/products/loading.tsx
+++ frontend/src/app/products/loading.tsx
@@ -1,18 +1,18 @@
 export default function Loading() {
   return (
-    <div className="container pt-16 pb-4">
+    <div className="container pt-8 pb-4 md:pt-16">
       <div className="flex animate-pulse">
         <div className="border-r border-neutral-200">
-          <div className="sticky top-8 h-[600px] w-64">
-            <div className="mr-8 flex h-full flex-col gap-4">
-              <div className="h-8 shrink-0 rounded-4xl bg-gray-200"></div>
-              <div className="h-full rounded-4xl bg-gray-200"></div>
+          <div className="xs:w-64 sticky top-8 w-36">
+            <div className="mr-4 flex flex-col gap-4 md:mr-8">
+              <div className="h-7 shrink-0 rounded-4xl bg-gray-200 md:h-8"></div>
+              <div className="h-96 rounded-4xl bg-gray-200"></div>
             </div>
           </div>
         </div>
-        <div className="ml-8 flex flex-1 flex-col gap-4">
+        <div className="ml-4 flex flex-1 flex-col gap-4 md:ml-8">
           <div className="flex items-center justify-between">
-            <div className="h-8 w-36 rounded-4xl bg-gray-200"></div>
+            <div className="h-7 w-36 rounded-4xl bg-gray-200 md:h-8"></div>
           </div>
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
             {[...Array(12)].map((_, index) => (
@@ -20,17 +20,17 @@ export default function Loading() {
                 <div className="flex aspect-square rounded-4xl bg-gray-200"></div>
                 <div className="flex h-full flex-col justify-between gap-4 pt-3">
                   <div className="flex flex-col gap-2">
-                    <div className="h-16 w-full rounded-4xl bg-gray-200"></div>
+                    <div className="h-14 w-full rounded-4xl bg-gray-200 md:h-16"></div>
                     <div className="flex gap-2">
-                      <div className="flex h-6 items-center gap-0.5">
-                        <div className="h-6 w-28 rounded-4xl bg-gray-200"></div>
+                      <div className="flex h-5 items-center gap-0.5 md:h-6">
+                        <div className="h-5 w-28 rounded-4xl bg-gray-200 md:h-6"></div>
                       </div>
-                      <div className="h-6 w-8 rounded-4xl bg-gray-200"></div>
+                      <div className="h-5 w-8 rounded-4xl bg-gray-200 md:h-6"></div>
                     </div>
                   </div>
                   <div className="flex flex-col gap-2">
                     <div className="h-7 w-12 rounded-4xl bg-gray-200"></div>
-                    <div className="h-12 w-full rounded-4xl bg-gray-200"></div>
+                    <div className="h-11 w-full rounded-4xl bg-gray-200 md:h-12"></div>
                   </div>
                 </div>
               </div>
