3e8fdac6be2fcc016c8592e844f1784536880bcd
marianobura <marianoobura@gmail.com>
2025-09-27 00:22:16 -0300

    Refactor loading component layout and styles for improved responsiveness and design consistency

--- frontend/src/app/products/[id]/loading.tsx
+++ frontend/src/app/products/[id]/loading.tsx
@@ -2,10 +2,12 @@ export default function Loading() {
   return (
     <>
       <main className="container flex animate-pulse flex-col justify-center">
-        <div className="flex items-center justify-center">
-          <div className="flex w-full items-center justify-center gap-4">
-            <div className="min-h-[640px] w-full flex-1 rounded-4xl bg-gray-200"></div>
-            <div className="flex h-full max-h-[640px] flex-1 flex-col justify-between gap-16 overflow-hidden">
+        <div className="my-3 flex items-center justify-center">
+          <div className="grid w-full grid-cols-1 items-center justify-center gap-4 sm:grid-cols-2">
+            <div className="flex h-full max-h-[500px] items-center justify-center overflow-hidden rounded-4xl bg-gray-200 xl:max-h-[640px]">
+              <div className="block h-80 max-h-80 bg-gray-300 object-contain sm:hidden sm:aspect-square sm:h-full sm:max-h-[500px]"></div>
+            </div>
+            <div className="flex h-full max-h-[500px] flex-col justify-between gap-4 overflow-hidden md:gap-8 lg:gap-16 xl:max-h-[640px]">
               <div className="flex flex-col gap-2">
                 <div className="h-6 w-16 rounded-4xl bg-gray-200"></div>
                 <div className="h-24 w-full rounded-4xl bg-gray-200"></div>
@@ -27,22 +29,22 @@ export default function Loading() {
         </div>
       </main>
       <footer className="animate-pulse border-t border-neutral-200">
-        <div className="container flex">
-          <div className="border-r border-neutral-200 p-10 pl-0">
-            <div className="h-6 w-28 rounded-4xl bg-gray-200"></div>
+        <div className="container flex flex-col sm:flex-row">
+          <div className="flex items-center justify-center border-neutral-200 pt-5 sm:border-r sm:pr-5 sm:pb-5 lg:p-10">
+            <div className="h-7 w-36 rounded-4xl bg-gray-200"></div>
           </div>
-          <div className="flex grow items-center justify-between gap-8 pl-10">
-            <div className="flex flex-col gap-1">
-              <div className="h-7 w-96 rounded-4xl bg-gray-200"></div>
-              <div className="flex gap-2">
-                <div className="h-6 w-44 rounded-4xl bg-gray-200"></div>
-                <div className="h-6 w-44 rounded-4xl bg-gray-200"></div>
-                <div className="h-6 w-44 rounded-4xl bg-gray-200"></div>
+          <div className="items-left flex grow flex-col justify-between gap-2 py-3 sm:py-5 sm:pl-5 lg:pl-10 xl:flex-row xl:items-center">
+            <div className="flex flex-col items-center gap-0.5 sm:items-start">
+              <div className="h-5 w-36 rounded-4xl bg-gray-200 md:h-6"></div>
+              <div className="flex flex-wrap justify-center gap-1 sm:justify-start">
+                <div className="h-5 w-32 rounded-4xl bg-gray-200 md:h-6"></div>
+                <div className="h-5 w-32 rounded-4xl bg-gray-200 md:h-6"></div>
+                <div className="h-5 w-32 rounded-4xl bg-gray-200 md:h-6"></div>
               </div>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="xs:flex-row xs:gap-4 flex flex-col items-center gap-2">
               <div className="h-7 w-16 rounded-4xl bg-gray-200"></div>
-              <div className="h-12 w-36 rounded-4xl bg-gray-200"></div>
+              <div className="h-11 w-full rounded-4xl bg-gray-200 sm:w-36 md:h-12"></div>
             </div>
           </div>
         </div>
